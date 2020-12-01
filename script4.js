f74045e6eea291b769334fc2d6163d01bb253128
Aleksandr Anisenko <aleksandr.anisenko@mail.ru>
2020-12-01 20:24:58 +0300

    start task 4

---
+++ script4.js
@@ -0,0 +1,20 @@
+// Task 5
+function valueAlert5() {
+    let value5 = document.getElementById('task5');
+    alert(value5.value);
+}
+
+// Task 6
+function changeValue6() {
+    let value6 = document.getElementById('task6');
+    value6.value = 'Ой я поменял свой текст!';
+}
+
+// Task 7
+function changeImg7() {
+    let img7 = document.getElementById('task7');
+    img7.src = 'https://i.pinimg.com/736x/7c/13/a8/7c13a8c9f5f2c28a7538bb8c7708f38e.jpg';
+}
+
+
+
